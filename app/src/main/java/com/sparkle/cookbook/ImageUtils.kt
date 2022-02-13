769c2a86cf1ebd8c356231479820504e903cf6c5
Indira Temkesheva <indiratemk@gmail.com>
2022-02-13 21:21:05 +0300

    create draft compose screen

---
+++ app/src/main/java/com/sparkle/cookbook/ImageUtils.kt
@@ -0,0 +1,30 @@
+package com.sparkle.cookbook
+
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+
+@Composable
+fun loadImage(url: String): MutableState<Bitmap?> {
+    val bitmapState: MutableState<Bitmap?> = remember { mutableStateOf(null) }
+
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(url)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                bitmapState.value = resource
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) = Unit
+        })
+
+    return bitmapState
+}
--- app/src/main/java/com/sparkle/cookbook/MainActivity.kt
+++ app/src/main/java/com/sparkle/cookbook/MainActivity.kt
@@ -3,36 +3,95 @@ package com.sparkle.cookbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.coroutineScope
 import com.sparkle.cookbook.ui.theme.CookBookTheme
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: RecipeViewModel by lazy {
+        RecipeViewModelFactory((application as CookBookApplication).database.recipeDao())
+            .create(RecipeViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycle.coroutineScope.launch {
+            viewModel.getRecipes().collect() { recipes ->
+                println(recipes)
+            }
+        }
+
         setContent {
             CookBookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                LazyColumn(content = {
+                    item { Title(getString(R.string.recipes_list_title)) }
+                    item { Recipes(viewModel = viewModel) }
+                })
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Title(title: String) {
+    Text(
+        text = title,
+        style = Typography().h4
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    CookBookTheme {
-        Greeting("Android")
-    }
+fun Recipes(viewModel: RecipeViewModel) {
+    val recipes = viewModel.getRecipes().collectAsState(initial = emptyList()).value
+
+    LazyRow(
+        content = {
+            item { Spacer(Modifier.size(8.dp)) }
+            for (recipe in recipes) {
+                item {
+                    Column(
+                        modifier = Modifier.width(120.dp)
+                    ) {
+                        val image = loadImage(url = recipe.imageUrl)
+                        image.value?.let { bitmap ->
+                            Image(
+                                bitmap = bitmap.asImageBitmap(),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .heightIn(min = 120.dp, max = 120.dp)
+                                    .widthIn(min = 120.dp, max = 120.dp)
+                                    .clip(RoundedCornerShape(8.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                        Text(text = recipe.title)
+                        Text(text = recipe.description)
+                    }
+
+                    Spacer(Modifier.size(8.dp))
+                }
+            }
+        }
+    )
 }
