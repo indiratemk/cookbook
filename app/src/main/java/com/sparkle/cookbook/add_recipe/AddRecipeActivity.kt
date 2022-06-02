2969500ca522d86e2767ce179303f73c3ecc3978
Indira Temkesheva <indiratemk@gmail.com>
2022-06-02 23:49:03 +0600

    add create recipe screen

--- app/src/main/java/com/sparkle/cookbook/add_recipe/AddRecipeActivity.kt
+++ app/src/main/java/com/sparkle/cookbook/add_recipe/AddRecipeActivity.kt
@@ -3,19 +3,18 @@ package com.sparkle.cookbook.add_recipe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.sparkle.cookbook.R
 import com.sparkle.cookbook.ui.theme.CookBookTheme
+import com.sparkle.cookbook.ui.theme.DefaultButton
+import com.sparkle.cookbook.ui.theme.MultiLineInput
+import com.sparkle.cookbook.ui.theme.SingleLineInput
 import com.sparkle.cookbook.ui.theme.Subtitle
 import com.sparkle.cookbook.ui.theme.Title
 
@@ -37,30 +36,32 @@ fun AddRecipeScreen() {
         item { Subtitle(text = LocalContext.current.getString(R.string.add_recipe_name)) }
         item {
             val titleInput = remember { mutableStateOf(TextFieldValue()) }
-            TextField(
-                value = titleInput.value,
-                onValueChange = { titleInput.value = it },
-                placeholder = {
-                    Text(
-                        text = LocalContext.current.getString(R.string.add_recipe_name_hint)
-                    )
-                },
-                modifier = Modifier.padding(start = 16.dp, bottom = 16.dp)
+            SingleLineInput(
+                input = titleInput,
+                hint = LocalContext.current.getString(R.string.add_recipe_name_hint),
+                startSpace = 16.dp,
+                bottomSpace = 16.dp,
+                endSpace = 16.dp
             )
         }
         item { Subtitle(text = LocalContext.current.getString(R.string.add_recipe_description)) }
         item {
             val descriptionInput = remember { mutableStateOf(TextFieldValue()) }
-            TextField(
-                value = descriptionInput.value,
-                onValueChange = { descriptionInput.value = it },
-                placeholder = {
-                    Text(
-                        text = LocalContext.current.getString(R.string.add_recipe_description_hint)
-                    )
-                },
-                modifier = Modifier.padding(start = 16.dp)
+            MultiLineInput(
+                input = descriptionInput,
+                hint = LocalContext.current.getString(R.string.add_recipe_description_hint),
+                startSpace = 16.dp,
+                bottomSpace = 16.dp,
+                endSpace = 16.dp
             )
+        }
+        item {
+            DefaultButton(
+                title = LocalContext.current.getString(R.string.add_recipe_create_button),
+                startSpace = 16.dp,
+                endSpace = 16.dp,
+                topSpace = 8.dp
+            ) {}
         }
     })
 }
--- app/src/main/java/com/sparkle/cookbook/ui/theme/Typeface.kt
+++ app/src/main/java/com/sparkle/cookbook/ui/theme/Typeface.kt
@@ -1,11 +1,21 @@
 package com.sparkle.cookbook.ui.theme
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -41,4 +51,102 @@ fun Body(text: String) {
         fontWeight = FontWeight.SemiBold,
         modifier = Modifier.padding(top = 8.dp)
     )
+}
+
+@Composable
+fun SingleLineInput(
+    input: MutableState<TextFieldValue>,
+    hint: String,
+    topSpace: Dp = 0.dp,
+    bottomSpace: Dp = 0.dp,
+    startSpace: Dp = 0.dp,
+    endSpace: Dp = 0.dp,
+) {
+    TextField(
+        value = input.value,
+        onValueChange = { input.value = it },
+        placeholder = {
+            Text(
+                text = hint
+            )
+        },
+        modifier = Modifier
+            .padding(
+                start = startSpace,
+                end = endSpace,
+                top = topSpace,
+                bottom = bottomSpace
+            ).fillMaxWidth(),
+        colors = TextFieldDefaults.textFieldColors(
+            cursorColor = Green,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        shape = Shapes.small,
+        singleLine = true
+    )
+}
+
+@Composable
+fun MultiLineInput(
+    input: MutableState<TextFieldValue>,
+    hint: String,
+    topSpace: Dp = 0.dp,
+    bottomSpace: Dp = 0.dp,
+    startSpace: Dp = 0.dp,
+    endSpace: Dp = 0.dp,
+) {
+    TextField(
+        value = input.value,
+        onValueChange = { input.value = it },
+        placeholder = {
+            Text(
+                text = hint
+            )
+        },
+        modifier = Modifier
+            .padding(
+                start = startSpace,
+                end = endSpace,
+                top = topSpace,
+                bottom = bottomSpace
+            ).fillMaxWidth()
+            .height(120.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            cursorColor = Green,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        shape = Shapes.small,
+        maxLines = 5,
+    )
+}
+
+@Composable
+fun DefaultButton(
+    title: String,
+    topSpace: Dp = 0.dp,
+    bottomSpace: Dp = 0.dp,
+    startSpace: Dp = 0.dp,
+    endSpace: Dp = 0.dp,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(
+                start = startSpace,
+                end = endSpace,
+                top = topSpace,
+                bottom = bottomSpace
+            ).fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Green,
+            contentColor = Color.White
+        ),
+        shape = Shapes.small
+    ) {
+        Text(text = title)
+    }
 }
